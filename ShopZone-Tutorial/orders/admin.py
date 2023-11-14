3dd8f74fcfc5921ee23cc8039ca3cf9d80aa5820
DjAlvaroCuba <famousalvarocubaporras@gmail.com>
2023-11-14 13:03:06 -0500

    NS Proyect

--- ShopZone-Tutorial/orders/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . models import Order, Orderitem, ShippingAddress
-
-admin.site.register(Order)
-admin.site.register(Orderitem)
-admin.site.register(ShippingAddress)
